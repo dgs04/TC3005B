9067093511c12ca4297c06e41b3afa7f6791afe1
dgs04 <A01285960@tec.mx>
2026-03-26 16:57:31 -0600

    first commit

---
+++ react/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+
+      </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
